0dee0fa138815d8979892b3221e6b362fe7a7f75
gabrieldenoni <gabrieldenonifake@gmail.com>
2022-12-24 14:49:18 -0300

    finish cards project

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -24,5 +31,39 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	byteSlice, error := ioutil.ReadFile(filename)
+
+	if error != nil {
+		fmt.Println("Error: ", error)
+		os.Exit(1)
+	}
+
+	cards := strings.Split(string(byteSlice), ",")
+	return deck(cards)
+}
+
+func (d deck) shuffle() {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range d {
+		newI := rand.Intn(len(d) - 1)
+
+		d[i], d[newI] = d[newI], d[i]
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
 }
